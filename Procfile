b990e6192d38b0ed6ffec838be098b67548993ef
windioo <windioya25@gmail.com>
2021-06-15 10:07:14 +0700

    update

--- Procfile
+++
@@ -1,1 +0,0 @@
-web : verndor/bin/heroku-php-apache2 public/
--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => '',
-		'password' => '',
-		'database' => '',
+		'username' => 'root',
+		'password' => 'windioya25',
+		'database' => 'cip',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
